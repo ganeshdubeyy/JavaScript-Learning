f94e101521a32f049d2166d0ee0418978bcf8b5a
Ganesh <ganesh.dubeyy@gmail.com>
2025-09-30 20:30:33 +0530

    Add obj.js file

---
+++ obj.js
@@ -0,0 +1,35 @@
+//Objects in JavaScript
+let person = {
+    name: "John",
+    age: 30,    
+    city: "New York"
+};
+//console.log(person);
+// console.log(person.name); // Accessing property
+// console.log(person["age"]); // Accessing property
+// person.age = 31; // Modifying property
+// console.log(person);
+// person.country = "USA"; // Adding property
+// console.log(person);
+// delete person.city; // Deleting property
+// console.log(person);
+// console.log(person);
+
+//Practice
+let book = {
+    title : "The Great Gatsby",
+    author : "F. Scott Fitzgerald",
+    pages : 180,
+}
+console.log(book);
+
+let student = {
+    name : "Alice",
+    marks : {
+        Math : 85,
+        English : 90,
+        Science : 95
+    }
+};
+console.log(student);
+console.log(student.marks.Math);
